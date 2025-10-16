1389b5ed8a418ac7709bb4208cae77b5d6b5c099
Ahmed El Mansy <ahmedmansi2004@gmail.com>
2025-10-16 17:19:37 +0300

    Added the frontend part

--- my-angular-app/src/app/app.ts
+++ my-angular-app/src/app/app.ts
@@ -5,8 +5,51 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
-  protected readonly title = signal('my-angular-app');
+  mainDisplayedNumber = "0";
+  subDisplay = "0";
+  //isOverWritable = false;
+
+  writeOnDisplay(num: string){
+    if (this.mainDisplayedNumber == "0") {
+      if (num != "0") {
+        this.mainDisplayedNumber = num;
+      }
+    }
+    else {
+      this.mainDisplayedNumber += num;
+    }
+  }
+
+  clear(){
+    this.mainDisplayedNumber = "0";
+  }
+
+  delete(){
+    this.mainDisplayedNumber = this.mainDisplayedNumber.slice(0,-1);
+    if (this.mainDisplayedNumber == "") {
+      this.mainDisplayedNumber = "0";
+    }
+  }
+
+  addPnt(){
+    if (!this.mainDisplayedNumber.includes(".")) {
+      this.mainDisplayedNumber += ".";
+    }
+  }
+
+  putSign(){
+    if (this.mainDisplayedNumber!="0") {
+      if (this.mainDisplayedNumber.startsWith("-")) {
+        this.mainDisplayedNumber = this.mainDisplayedNumber.slice(1);
+      }
+      else{
+        this.mainDisplayedNumber = "-" + this.mainDisplayedNumber;
+      }
+    }
+    
+  }
+
 }
